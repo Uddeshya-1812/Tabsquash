501cf5a01a3884009af056829a2b1d071c5ed42d
Uddeshya-1812 <uddeshya1812@gmail.com>
2022-01-09 18:30:23 +0530

    source code refactored

--- popup.js
+++ popup.js
@@ -1,47 +1,60 @@
+const SPLITTER = '#';
+const ZERO_INDEX  = 0;
+const ZERO_FLAG = 0;
+const ONE_FLAG = 1;
+let INITIAL_URL_COUNT = 1;
+
+
 let squashButton = document.getElementById('squash-button');
-
 squashButton.addEventListener("click",async ()=>{
-    // squashing logic
-
-    // getting all tabs 
     let allTabs = await chrome.tabs.query({
         active: false,
         currentWindow: true
     });
-    let urls = [];
-    let ids = [];
-    // getting tab ids
-    for (let  i = 0; i < allTabs.length ; i ++){
-        if (urls.includes(allTabs[i].url)) ids.push(allTabs[i].id)
-        else urls.push(allTabs[i].url);
-    }
+    allTabs[allTabs.length] = (await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }))[ZERO_INDEX]
+    
     // squashing Tabs
-    let removeTabPromise = chrome.tabs.remove(ids)
+    let ids = return_tab_ids(allTabs, ZERO_FLAG)
+    chrome.tabs.remove(ids);
 
 })
 
 let squashdiffPoint = document.getElementById('squash-diff-button');
 squashdiffPoint.addEventListener("click",async ()=>{
     // squashing logic
-
-    // getting all tabs 
     let allTabs = await chrome.tabs.query({
         active: false,
         currentWindow: true
     });
-    let urls = [];
-    let ids = [];
-
-    // getting tab ids
-    for (let  i = 0; i < allTabs.length ; i ++){
-        let tabUrl = allTabs[i].url.split('#')[0];
-        
-        if (urls.includes(tabUrl)) ids.push(allTabs[i].id)
-
-        else urls.push(tabUrl);
-        
-    }
-    // squashing Tabs
-    let removeTabPromise = chrome.tabs.remove(ids)
+    allTabs[allTabs.length] = (await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }))[ZERO_INDEX]
+    
+    let ids = return_tab_ids(allTabs, ONE_FLAG)
+    chrome.tabs.remove(ids);
 
 })
+
+function return_tab_ids(allTabs,flag){
+    urlCounter = {};
+    removeTabIds = [];
+    let tabUrl;
+    for(let i = 0; i < allTabs.length; i++){
+        if(flag){
+            tabUrl = allTabs[i].url.split(SPLITTER)[ZERO_INDEX];
+        }
+        else{
+            tabUrl = allTabs[i].url
+        }
+        if(urlCounter[tabUrl]){
+            removeTabIds.push(allTabs[i].id)
+        }
+        else urlCounter[tabUrl] = INITIAL_URL_COUNT;
+    }
+
+    return removeTabIds
+}
